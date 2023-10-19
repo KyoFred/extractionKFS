bcdd0830e8caadbc93cb576de5cf169c6cdf8c72
KyoFred <freddy.herrera@dit.kyocera.com>
2023-10-19 10:08:23 +0200

    Refactor index

--- src/index.cjs
+++ src/index.cjs
@@ -1,32 +1,36 @@
-const { replaceDotWithComma } = require('./replaceDotWithComma.cjs');
-const { connectToSqlServer} = require('./connectToHda.cjs');
+const { listDevices } = require('./listDevices.cjs');
 const { processDevices } = require('./processDevices.cjs');
-
 const fs = require('fs').promises;
 const path = require('path');
 
 const settingsFilePath = path.resolve(__dirname, '../data/settings.json');
 const jsonFilePath = path.resolve(__dirname, '../coverages/');
-const listDeviceFilePath = path.resolve(__dirname, '../data/listDevice.json');
+const listDeviceFilePath = path.resolve(__dirname, '../data/listDevices.json');
 const listDeviceFilePathUpdate = path.resolve(__dirname, '../data/listDeviceUpdate.json');
 
-
-
-async function connect() {
+async function startProcessKfs() {
   try {
-   const data = await fs.readFile(settingsFilePath, 'utf8');
-        const settings = JSON.parse(data);
+    const data = await fs.readFile(settingsFilePath, 'utf8');
+    const settings = JSON.parse(data);
     const { userId, password, pageLogin, pageKfs } = settings;
 
-   // connectToSqlServer(settingsFilePath,listDeviceFilePath);
-   const deviceData = await fs.readFile(listDeviceFilePath, 'utf8');
-   // processDevices(userId,password,pageLogin,pageKfs,deviceData,jsonFilePath,listDeviceFilePathUpdate,fs);
-  replaceDotWithComma(listDeviceFilePathUpdate, fs);
-    // lista device 
-     } catch (error) {
-    console.error('Errore:', error);
-          console.error('Problema di connessione, numero massimo di tentativi raggiunto.');
-    }
-  }
+    const listDeviceFileExists = await fs.access(listDeviceFilePath)
+      .then(() => true)
+      .catch(() => false);
 
-connect();
+    if (!listDeviceFileExists) {
+      console.error('Il file listDevices.json non esiste');
+      return;
+    }
+    // Questo codice esegue un'operazione asincrona per elencare i dispositivi 
+   // await listDevices(listDeviceFilePath);
+
+    const deviceData = await fs.readFile(listDeviceFilePath, 'utf8');
+// Attende il processo dei dispositivi utilizzando le credenziali dell'utente fornite per l'accesso
+   await processDevices(userId, password, pageLogin, pageKfs, deviceData, jsonFilePath, listDeviceFilePathUpdate);
+  } catch (error) {
+    console.error('Errore nella lettura del file:', error);
+  }
+}
+
+startProcessKfs();
