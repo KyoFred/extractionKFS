4e1bad9d609edf36635d2ba302ea006bb0e70393
kyo-cera <KNM-Licenses@dit.kyocera.com>
2023-11-21 16:08:56 +0100

    clean code

--- src/processDevices.cjs
+++ src/processDevices.cjs
@@ -10,8 +10,7 @@ async function processDevices(userId, password, pageLogin, pageKfs, jsonFilePath
     const driver = await new Builder().forBrowser('chrome').build();
     try {
       await driver.get(pageLogin);
-       await setCookie(driver);
-      console.log('set cookie'); // Imposta i cookie nel browser
+       await setCookie(driver); // Imposta i cookie nel browser
       await driver.navigate().refresh(); // Aggiorna la pagina per applicare i cookie 
       await driver.findElement(By.id('user-id')).sendKeys(userId);
       await driver.findElement(By.id('password')).sendKeys(password);
@@ -19,10 +18,10 @@ async function processDevices(userId, password, pageLogin, pageKfs, jsonFilePath
       await driver.wait(until.urlIs(pageLogin), TIMEOUT);
       const loginElement = await driver.wait(until.elementLocated(By.id('hd-login-user-name')), TIMEOUT);
       if (!loginElement) {
-        console.log('Accesso non riuscito.');
+       // console.log('Accesso non riuscito.');
         return false;
       } else {
-        console.log('Accesso riuscito.');
+       // console.log('Accesso riuscito.');
         const jsonDataArray = [];
         const jsonDataError = [];
         for (const device of devices) {
@@ -42,9 +41,9 @@ async function processDevices(userId, password, pageLogin, pageKfs, jsonFilePath
                 if (jsonData === null) {
                   device['status'] = 'null';
                   device['data'] = {};
-                  console.log('jsonData null ', jsonData);
+                //  console.log('jsonData null ', jsonData);
                 } else {
-                  console.log('jsonData ok ', jsonData);
+                 // console.log('jsonData ok ', jsonData);
                   device['status'] = 'ok';
                   device['data'] = jsonData;
                 }
@@ -52,7 +51,7 @@ async function processDevices(userId, password, pageLogin, pageKfs, jsonFilePath
                 break; // Esci dal ciclo while in caso di successo
               } catch (error) {
                 if (error.name === 'NoSuchElementError') {
-                  console.log('Elemento #coverages non trovato. Salvataggio del testo dai tag h1 e h2.');
+                //  console.log('Elemento #coverages non trovato. Salvataggio del testo dai tag h1 e h2.');
                   const h1Text = await driver.findElement(By.tagName('h1')).getText();
                   const h2Text = await driver.findElement(By.tagName('h2')).getText();
                   const errorData = { h1: h1Text, h2: h2Text, DeviceId: DeviceId };
@@ -62,7 +61,7 @@ async function processDevices(userId, password, pageLogin, pageKfs, jsonFilePath
                   device['errorMessage'] = h2Text;
                   break; // Esci dal ciclo while in caso di errore
                 } else {
-                  console.log('jsonDataArray: -->', jsonDataArray);
+                 // console.log('jsonDataArray: -->', jsonDataArray);
                 }
               }
             } catch (error) {
@@ -73,13 +72,12 @@ async function processDevices(userId, password, pageLogin, pageKfs, jsonFilePath
         }
         await fs.writeFile(listDeviceFilePathUpdate, JSON.stringify(devices, null, 2), 'utf8');
         await fs.writeFile(jsonFilePathError, JSON.stringify(jsonDataError, null, 2), 'utf8');
-        console.log('JSON salvato con successo nel file:', listDeviceFilePathUpdate);
+      //  console.log('JSON salvato con successo nel file:', listDeviceFilePathUpdate);
       }
     } catch (error) {
       console.error('Errore durante il processo di login:', error);
     } finally {
       console.log('chiude la sessione');
-      alert("chiude session");
       await driver.quit();
     }
   } catch (error) {
@@ -91,7 +89,7 @@ async function leggiJSON(nomeFile) {
   try {
     const contenutoFile = await fs.readFile(nomeFile, 'utf8');
     const datiJSON = JSON.parse(contenutoFile);
-    console.log("devices:", datiJSON[0]['DeviceId']);
+  //  console.log("devices:", datiJSON[0]['DeviceId']);
     return datiJSON;
   } catch (error) {
     console.error('Errore durante la lettura del file JSON:', error);
