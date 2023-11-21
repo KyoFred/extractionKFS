c307d705d959eda272aca7d73f06f7e1e7b2ab69
KyoFred <freddy.herrera@dit.kyocera.com>
2023-11-21 19:43:17 +0100

    create  api update & get

--- src/api/hdaCoverage.cjs
+++ src/api/hdaCoverage.cjs
@@ -1,8 +1,10 @@
 const express = require('express');
 const sql = require('mssql');
 const path = require('path');
+const { promises: fs } = require('fs');
+const { URL } = require('url');
+
 const settingsFilePath = path.resolve(__dirname, '../../data/settings.json');
-const fs = require('fs').promises;
 
 const leggiDatiDaFile = async () => {
   try {
@@ -26,38 +28,66 @@ const avviaServer = async () => {
         encrypt: false
       }
     };
-
     const pool = new sql.ConnectionPool(config);
     await pool.connect();
-
     const app = express();
     app.use(express.json());
-
     const queryBD = settings.queryDbListDeviceC;
     app.get('/api/listDevice', async (req, res) => {
       try {
-        const result = await pool.request().query(`SELECT * FROM TABUserDef_DataForm_P22C WHERE DeviceId = 'PD_KYRFC9Y20946';`);
+        const result = await pool.request().query(queryBD);
         res.send(result.recordset);
       } catch (error) {
         console.error(error);
         res.status(500).send('Errore nel recupero dei dati.');
       }
     });
-
-    app.put('/api/dati/:id', async (req, res) => {
+    app.put('/api/deviceUpdate/:id', async (req, res) => {
       try {
-        const query = 'UPDATE tabella SET campo = @campo WHERE id = @id';
-        const result = await pool.request()
-          .input('campo', sql.VarChar, req.body.campo)
-          .input('id', sql.Int, req.params.id)
-          .query(query);
-        res.send('Dati aggiornati correttamente.');
+        const jsonData = req.body;
+        const jsonDataString = JSON.stringify(jsonData.data);
+        const id = req.params.id;
+       // console.log('id:',id );
+        const query = `
+          UPDATE TABUserDef_DataForm_P22C 
+          SET lastUpdate = '${jsonData.data.lastUpdate}',
+              startPeriod = '${jsonData.data.startPeriod}',
+              endPeriod = '${jsonData.data.endPeriod}',
+              blackTotalAverage = '${jsonData.data.coverageData.blackTotalAverage}',
+              blackTotalUsagePage = '${jsonData.data.coverageData.blackTotalUsagePage}',
+              cyanTotalAverage = '${jsonData.data.coverageData.cyanTotalAverage}',
+              cyanTotalUsagePage = '${jsonData.data.coverageData.cyanTotalUsagePage}',
+              magentaTotalAverage = '${jsonData.data.coverageData.magentaTotalAverage}',
+              magentaTotalUsagePage = '${jsonData.data.coverageData.magentaTotalUsagePage}',
+              yellowTotalAverage = '${jsonData.data.coverageData.yellowTotalAverage}',
+              yellowTotalUsagePage = '${jsonData.data.coverageData.yellowTotalUsagePage}',
+              blackTotalCopyAverage = '${jsonData.data.coverageData.blackTotalCopyAverage}',
+              blackTotalCopyUsagePage = '${jsonData.data.coverageData.blackTotalCopyUsagePage}',
+              cyanCopyAverage = '${jsonData.data.coverageData.cyanCopyAverage}',
+              cyanCopyUsagePage = '${jsonData.data.coverageData.cyanCopyUsagePage}',
+              magentaCopyAverage = '${jsonData.data.coverageData.magentaCopyAverage}',
+              magentaCopyUsagePage = '${jsonData.data.coverageData.magentaCopyUsagePage}',
+              yellowCopyAverage = '${jsonData.data.coverageData.yellowCopyAverage}',
+              yellowCopyUsagePage = '${jsonData.data.coverageData.yellowCopyUsagePage}',
+              blackTotalFAXAverage = '${jsonData.data.coverageData.blackTotalFAXAverage}',
+              blackTotalFAXUsagePage = '${jsonData.data.coverageData.blackTotalFAXUsagePage}',
+              blackTotalPrinterAverage = '${jsonData.data.coverageData.blackTotalPrinterAverage}',
+              blackTotalPrinterUsagePage = '${jsonData.data.coverageData.blackTotalPrinterUsagePage}',
+              cyanPrinterAverage = '${jsonData.data.coverageData.cyanPrinterAverage}',
+              cyanPrinterUsagePage = '${jsonData.data.coverageData.cyanPrinterUsagePage}',
+              magentaPrinterAverage = '${jsonData.data.coverageData.magentaPrinterAverage}',
+              magentaPrinterUsagePage = '${jsonData.data.coverageData.magentaPrinterUsagePage}',
+              yellowPrinterAverage = '${jsonData.data.coverageData.yellowPrinterAverage}',
+              yellowPrinterUsagePage = '${jsonData.data.coverageData.yellowPrinterUsagePage}',
+              completejson = '${jsonDataString}'
+          WHERE DeviceId = '${id}' `;
+        const result = await pool.request().query(query);
+        res.status(200).send({ message: `Dati inseriti correttamente - ${jsonData.DeviceId} `, result: result });
       } catch (error) {
         console.error(error);
-        res.status(500).send('Errore nell\'aggiornamento dei dati.');
+        res.status(500).send('Errore nell\'aggiornamento dei dati: ', error);
       }
     });
-
     app.post('/api/inDevice', async (req, res) => {
       const jsonData = req.body;
       const jsonDataString = JSON.stringify(jsonData.data);
@@ -67,19 +97,17 @@ const avviaServer = async () => {
         DECLARE @sPrefix nvarchar(1) = 'X'
         EXEC [dbo].[spHDANET_GetNewID] @sParam, @sValue OUTPUT, @sPrefix
         INSERT INTO TABUserDef_DataForm_P22C (IDProtocollo, DeviceID, lastUpdate, startPeriod, endPeriod, blackTotalAverage, blackTotalUsagePage, cyanTotalAverage, cyanTotalUsagePage, magentaTotalAverage, magentaTotalUsagePage, yellowTotalAverage, yellowTotalUsagePage, blackTotalCopyAverage, blackTotalCopyUsagePage, cyanCopyAverage, cyanCopyUsagePage, magentaCopyAverage, magentaCopyUsagePage, yellowCopyAverage, yellowCopyUsagePage, blackTotalFAXAverage, blackTotalFAXUsagePage, blackTotalPrinterAverage, blackTotalPrinterUsagePage, cyanPrinterAverage, cyanPrinterUsagePage, magentaPrinterAverage, magentaPrinterUsagePage, yellowPrinterAverage, yellowPrinterUsagePage,completejson)
-        VALUES (@sValue, '${jsonData.DeviceId}', '${jsonData.data.lastUpdate}', '${jsonData.data.startPeriod}', '${jsonData.data.endPeriod}', '${jsonData.data.coverageData.blackTotalAverage}', '${jsonData.data.coverageData.blackTotalUsagePage}', '${jsonData.data.coverageData.cyanTotalAverage}', '${jsonData.data.coverageData.cyanTotalUsagePage}', '${jsonData.data.coverageData.magentaTotalAverage}', '${jsonData.data.coverageData.magentaTotalUsagePage}', '${jsonData.data.coverageData.yellowTotalAverage}', '${jsonData.data.coverageData.yellowTotalUsagePage}', '${jsonData.data.coverageData.blackTotalCopyAverage}', '${jsonData.data.coverageData.blackTotalCopyUsagePage}', '${jsonData.data.coverageData.cyanCopyAverage}', '${jsonData.data.coverageData.cyanCopyUsagePage}', '${jsonData.data.coverageData.magentaCopyAverage}', '${jsonData.data.coverageData.magentaCopyUsagePage}', '${jsonData.data.coverageData.yellowCopyAverage}', '${jsonData.data.coverageData.yellowCopyUsagePage}', '${jsonData.data.coverageData.blackTotalFAXAverage}', '${jsonData.data.coverageData.blackTotalFAXUsagePage}', '${jsonData.data.coverageData.blackTotalPrinterAverage}', '${jsonData.data.coverageData.blackTotalPrinterUsagePage}', '${jsonData.data.coverageData.cyanPrinterAverage}', '${jsonData.data.coverageData.cyanPrinterUsagePage}', '${jsonData.data.coverageData.magentaPrinterAverage}', '${jsonData.data.coverageData.magentaPrinterUsagePage}', '${jsonData.data.coverageData.yellowPrinterAverage}', '${jsonData.data.coverageData.yellowPrinterUsagePage}','${jsonDataString}
-        ')`;
-    
+        VALUES (@sValue, '${jsonData.DeviceId}', '${jsonData.data.lastUpdate}', '${jsonData.data.startPeriod}', '${jsonData.data.endPeriod}', '${jsonData.data.coverageData.blackTotalAverage}', '${jsonData.data.coverageData.blackTotalUsagePage}', '${jsonData.data.coverageData.cyanTotalAverage}', '${jsonData.data.coverageData.cyanTotalUsagePage}', '${jsonData.data.coverageData.magentaTotalAverage}', '${jsonData.data.coverageData.magentaTotalUsagePage}', '${jsonData.data.coverageData.yellowTotalAverage}', '${jsonData.data.coverageData.yellowTotalUsagePage}', '${jsonData.data.coverageData.blackTotalCopyAverage}', '${jsonData.data.coverageData.blackTotalCopyUsagePage}', '${jsonData.data.coverageData.cyanCopyAverage}', '${jsonData.data.coverageData.cyanCopyUsagePage}', '${jsonData.data.coverageData.magentaCopyAverage}', '${jsonData.data.coverageData.magentaCopyUsagePage}', '${jsonData.data.coverageData.yellowCopyAverage}', '${jsonData.data.coverageData.yellowCopyUsagePage}', '${jsonData.data.coverageData.blackTotalFAXAverage}', '${jsonData.data.coverageData.blackTotalFAXUsagePage}', '${jsonData.data.coverageData.blackTotalPrinterAverage}', '${jsonData.data.coverageData.blackTotalPrinterUsagePage}', '${jsonData.data.coverageData.cyanPrinterAverage}', '${jsonData.data.coverageData.cyanPrinterUsagePage}', '${jsonData.data.coverageData.magentaPrinterAverage}', '${jsonData.data.coverageData.magentaPrinterUsagePage}', '${jsonData.data.coverageData.yellowPrinterAverage}', '${jsonData.data.coverageData.yellowPrinterUsagePage}','${jsonDataString}'
+      `;
       try {
         const result2 = await pool.request().query(query);
         res.status(200).send({ message: `Dati inseriti correttamente - ${jsonData.DeviceId} `, result: result2 });
       } catch (error) {
         console.error(error);
-        res.status(500).send(`Errore nell inserimento dei dati: ${jsonData.DeviceId} ` , error.message);
+        res.status(500).send(`Errore nell'inserimento dei dati: ${jsonData.DeviceId} `, error.message);
       }
     });
-
-    app.delete('/api/dati/:id', async (req, res) => {
+    app.delete('/api/deviceDelete/:id', async (req, res) => {
       try {
         const query = 'DELETE FROM tabella WHERE id = @id';
         const result = await pool.request()
@@ -91,7 +119,18 @@ const avviaServer = async () => {
         res.status(500).send('Errore nell\'eliminazione dei dati.');
       }
     });
-
+    app.get('/api/device/:id', async (req, res) => {
+      try {
+        const query = 'SELECT * FROM TABUserDef_DataForm_P22C WHERE DeviceId = @id';
+        const result = await pool.request()
+          .input('id', sql.VarChar, req.params.id)
+          .query(query);
+        res.send(result.recordset);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send('Errore nel recupero dei dati.');
+      }
+    });
     const port = 3001;
     app.listen(port, () => {
       console.log(`API in ascolto sulla porta ${port}`);
