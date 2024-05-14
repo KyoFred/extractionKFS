bdfc4769c01a07cfac50fd7a3db09ac8eb26b9e5
KyoFred <freddy.herrera@dit.kyocera.com>
2024-05-14 19:34:58 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import fs from 'fs/promises';
+import cron from 'node-cron';
 import { insertToDB } from './InsertToDB.cjs';
 import { avviaServer } from './api/hdaCoverage.cjs';
 import { writeToLog } from './writeToLog.cjs';
 
-// ... resto del codice
-
-
 const settingsFilePath = new URL('../data/settings.json', import.meta.url).pathname;
 const jsonFilePath = new URL('../coverages/', import.meta.url).pathname; 
 const listDevicePath = new URL('../data/listDevices.json', import.meta.url).pathname; 
-const listDeviceFilePathUpdate = new URL('../data/listDeviceUpdate.json', import.meta.url).pathname;   
-// Percorso del file di log
+const listDeviceFilePathUpdate = new URL('../data/listDeviceUpdate.json', import.meta.url).pathname;  
 
-
-
-
-
-
-async function startProcessKfs() {
+cron.schedule('16 18 * * 0', async () => {
   try {
     const settingsData = await fs.readFile(settingsFilePath, 'utf8');
     const settings = JSON.parse(settingsData);
@@ -25,9 +17,8 @@ async function startProcessKfs() {
 
     try {
       console.log('---ListDevices Start ---');
-    // await listDevices(listDevicePath);
+      await listDevices(listDevicePath);
       console.log('--- ListDevices fine ---');
-      
     } catch (error) {
       console.error('Errore durante l\'esecuzione di listDevices:', error);
       writeToLog('Errore durante l\'esecuzione di listDevices:', { error });
@@ -35,7 +26,7 @@ async function startProcessKfs() {
 
     try {
       console.log('--- Process Devices start---');
-   //  await processDevices(userId, password, pageLogin, pageKfs, jsonFilePath, listDeviceFilePathUpdate,listDevicePath);
+      await processDevices(userId, password, pageLogin, pageKfs, jsonFilePath, listDeviceFilePathUpdate, listDevicePath);
       console.log('--- Process Devices fine ---');
     } catch (error) {
       console.error('Errore durante l\'esecuzione di processDevices:', error);
@@ -44,8 +35,7 @@ async function startProcessKfs() {
 
     try {
       console.log('--- InsertToDB start---');
-      const q = true;
-      await insertToDB(q);
+      await insertToDB();
       console.log('--- InsertToDB fine ---');
     } catch (error) {
       console.error('Errore durante l\'esecuzione di InsertToDB:', error);
@@ -55,9 +45,6 @@ async function startProcessKfs() {
     console.error('Errore index --->:', error);
     writeToLog('Errore index --->:', { error });
   }
-}
+});
 
-//startProcessKfs();
 avviaServer();
- cron.schedule('16 18 * * 0', () => {  startProcessKfs();});
- 
