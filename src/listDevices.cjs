be3c30b0cf7cdeb17b817e1b9d89af195663c66e
KyoFred <freddy.herrera@dit.kyocera.com>
2024-05-15 15:39:42 +0200

    add api group for the specified group

--- src/listDevices.cjs
+++ src/listDevices.cjs
@@ -3,17 +3,30 @@ const axios = require('axios');
 
 async function listDevices(listDeviceFilePath) {
   try {
-    const response = await axios.get('http://localhost:4001/api/listDevice', { timeout: 5000 });
+    const response = await axios.get('http://localhost:4001/api/listDevice');
     console.log('Risposta:', response.data);
     const jsonData = JSON.stringify(response.data); // Converti l'array in una stringa JSON
     fs.writeFileSync(listDeviceFilePath, jsonData);
     console.log(`Dati della query salvati nel file listDevices.json. Numero di record salvati: ${response.data.length}`);
+    return response.data.length;
+  } catch (error) {
+    console.error(' listDevices Errore durante la richiesta:', error.message);
+  }
+}
+async function listDevicesGroup(listDeviceFilePath,group) {
+  try {
+    const response = await axios.get(`http://localhost:4001/api/listDeviceGroup/${group}`);
+    //console.log('Risposta:', response.data);
+    const jsonData = JSON.stringify(response.data); // Converti l'array in una stringa JSON
+    fs.writeFileSync(listDeviceFilePath, jsonData);
+    console.log(`Dati della query salvati nel file listDevices.json. Numero di record salvati: ${response.data.length}`);
+    return response.data.length;
   } catch (error) {
     console.error('Errore durante la richiesta:', error.message);
   }
 }
 
 module.exports = {
-  listDevices
+  listDevices,listDevicesGroup
 };
 
